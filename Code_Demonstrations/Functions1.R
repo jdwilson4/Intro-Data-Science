58b90703f4e2d99304ab3d39d343b9e249d30f94
James D. Wilson <jdwilson1212@gmail.com>
2017-10-24 13:52:28 -0700

    Added Functions1.R and Assignment 12

---
+++ Code_Demonstrations/Functions1.R
@@ -0,0 +1,99 @@
+#Loops and Functional Programming
+
+#for loops
+
+for(i in 1:5) print("BSDS 100")
+
+#for the safe side, let's use brackets. This is equivalent to the above
+for(i in 1:5){
+  print("BSDS 100")
+}
+
+#what's happening with i?
+
+for(i in 1:5){
+  print(i)
+  print("BSDS 100")
+}
+
+#we don't have to iterate numerically, instead e.g., we can go through all 
+#levels in a factor
+
+values <- factor(c("A", "A", "B", "C", "C", "C", "ZzZ"))
+levels(values)
+for(k in levels(values)){
+  print(k)
+}
+
+#we can update the seq for the looping variable and it won't change the originally
+#specified seq.
+
+for(i in 1:10){
+  i = i + 10
+  print(i)
+}
+
+##while loops
+#printing "MSAN 3 times"
+n <- 3
+while(n > 0){
+  print("MSAN")
+  n <- n - 1
+}
+
+#contrast this with the following
+
+n <- 3
+while(n > 0){
+  n <- n - 1
+  print("MSAN")
+}
+
+#what happens in the following?
+n <- 3
+while(n > 0){
+  n <- n + 1
+  print("MSAN")
+}
+
+
+#repeat loops
+# will keep executing what is inside the loop until a "break" condition is met
+
+x <- 7
+repeat{
+  print(x)
+  x <- x + 2
+  if(x > 10) break
+}
+
+#the above is equivalent to the following while() loop
+x <- 7
+while(x <= 10){
+  print(x)
+  x <- x + 2
+}
+
+
+x <- -3
+#if else statement
+if (x > 0){
+  print("x is positive")
+} else{ #else must begin on the same line for which the if{} statement ends
+  print("x is negative")
+}
+
+
+#if else if statements
+
+x <- 0
+x <- -2
+x <- 2
+x <- "a"
+if(x > 0){
+  print("x is positive")
+}else if(x < 0){
+  print("x is negative")
+}else{
+  print("x is zero")
+}
